2295cd1eda28bfa5bd5db5bac953033b3adc3faa
Thiago Silva <39065653+thdsilva@users.noreply.github.com>
2019-08-31 19:21:12 -0300

    Arquivos upados

---
+++ Strings/Strings.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    //exemplo Strings
+       char str [40];
+      printf ("\nDigite a String desejada= ");
+      gets (str);
+      printf("Sua string e: ");
+      puts(str);
+
+
+
+    return 0;
+}
---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int  main(){
+
+    int test;
+    char palavra [5];
+    printf("Digite um Numero : ");
+    scanf("%d", &test);
+
+    printf("Numero = %d", test);
+
+
+    
+}
